df063c788059b4df5a032cdc30e53c8058d9ee1f
Lucas <lucaslpmoura@gmail.com>
2026-05-06 10:39:42 -0300

    Added disconnect callback when application is stopped

--- app/src/main/java/com/lucaslpmoura/android_chat/MainApplication.kt
+++ app/src/main/java/com/lucaslpmoura/android_chat/MainApplication.kt
@@ -1,19 +1,51 @@
 package com.lucaslpmoura.android_chat
 
 import android.app.Application
+import androidx.compose.runtime.currentRecomposeScope
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
 import com.lucaslpmoura.android_chat.dependencies.appModule
+import com.lucaslpmoura.kotlin_chat.client.KotlinChatClient
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
+import org.koin.android.ext.android.getKoin
+import org.koin.android.ext.android.inject
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
+import org.koin.core.component.KoinComponent
 import org.koin.core.context.GlobalContext.startKoin
 
-class MainApplication : Application() {
+class MainApplication : Application(), KoinComponent, DefaultLifecycleObserver{
+
+    lateinit var chatClient: KotlinChatClient
+    private val applicationScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
+
     override fun onCreate() {
-        super.onCreate()
+        super<Application>.onCreate()
 
         startKoin{
             androidLogger()
             androidContext(this@MainApplication)
             modules(appModule)
         }
+
+        chatClient = getKoin().get()
+    }
+
+    override fun onStop(owner: LifecycleOwner) {
+        super.onStop(owner)
+
+
+        applicationScope.launch {
+            try{
+                println("Sending DISCONNECT to server....")
+                chatClient.disconnect()
+            } catch (e : Exception){
+                println(e.message)
+            }
+        }
+
     }
 }
